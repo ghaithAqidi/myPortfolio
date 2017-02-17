db84fae9462f9ec0a7dc2a6d76e0796746bdd85e
Ghaith Ali <ghaithammar2008@gmail.com>
2017-02-17 01:54:31 +0100

    fixed the active navigation item when pressing.

--- index.js
+++ index.js
@@ -11,4 +11,21 @@ $(document).ready(function(){
             });
         }
     });
+
+    //set the active class on the right li:
+    $("li.nav-item").on('click', function(){
+        var clickedLi = $(this);
+        var otherLis = $("li.nav-item");
+        otherLis.removeClass("active");
+        clickedLi.addClass("active");
+    });
+
+    //set the active class on home when clicking on "My Portfilio":
+    $("#myNavbar a.navbar-brand").on('click', function(){
+       var homeLi = $("li.nav-item:first");
+        var otherLis = $("li.nav-item");
+        otherLis.removeClass("active");
+        homeLi.addClass("active");
+
+    });
 });
